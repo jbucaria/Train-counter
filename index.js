6990e917a646ff7843c7cfcab1184dea51c7879f
jbucaria <157171759+jbucaria@users.noreply.github.com>
2024-03-19 13:36:25 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 let count = 0
+let counts = []
 
 let countEl = document.getElementById('count-el')
+let saveEl = document.getElementById('save-el')
+let totalEl = document.getElementById('total-el')
+let messageEl = document.getElementById('message')
 
 function increment() {
   count += 1
   countEl.textContent = count
+  messageEl.textContent = ''
 }
 
 function save() {
-  console.log(count)
+  if (count === 0) {
+    messageEl.textContent = 'No Passangers'
+  } else {
+    saveEl.textContent += ` ${count} - `
+    counts.push(count)
+    messageEl.textContent = 'Count saved successfully'
+    countEl.textContent = 0
+    count = 0
+  }
 }
 
-let saveEl = document.getElementById('save-el')
+function total() {
+  let totalCount = 0
+  for (let num of counts) {
+    totalCount += num
+  }
+  totalEl.textContent = `Total Passangers: ${totalCount}`
 
-function save() {
-  let newCount = count + ' - '
-  saveEl.textContent += newCount
-  countEl.textContent = 0
+  setTimeout(function () {
+    messageEl.textContent = ''
+  }, 1500)
 }
